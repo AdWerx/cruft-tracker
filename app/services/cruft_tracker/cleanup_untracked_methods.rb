ad78557ca5e1bf91e80036f705deb2e1d6d1e7c1
Doug Hughes <doug@doughughes.net>
2022-04-22 13:49:19 -0400

    Suppress logging when cleaning up tracked methods

--- app/services/cruft_tracker/cleanup_untracked_methods.rb
+++ app/services/cruft_tracker/cleanup_untracked_methods.rb
@@ -5,13 +5,15 @@ module CruftTracker
     private
 
     def execute
-      CruftTracker::Method
-        .where(deleted_at: nil)
-        .each do |method|
-          unless method.still_exists? && method.still_tracked?
-            method.update(deleted_at: Time.current)
+      CruftTracker::LogSuppressor.suppress_logging do
+        CruftTracker::Method
+          .where(deleted_at: nil)
+          .each do |method|
+            unless method.still_exists? && method.still_tracked?
+              method.update(deleted_at: Time.current)
+            end
           end
-        end
+      end
     rescue StandardError
       # I'm actively ignoring all errors. Chances are, these are due to something like running rake
       # tasks in CI when the DB doesn't already exist.
